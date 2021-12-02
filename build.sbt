51e328fe3f567438c52d68b40d14e2eeb9faf5d1
Charlie Saunders <charlieasaunders@gmail.com>
2021-12-01 21:53:34 -0500

    Initial commit. Day 1 complete.

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "advent-of-code-2021"
+scalaVersion := "2.13.7"
+organization := "com.charliesaunders.aoc2021"
+version := "1.0"
+
+scalacOptions += "-deprecation"
---
+++ src/main/scala/day1.scala
@@ -0,0 +1,23 @@
+package day1
+
+object Main extends App {
+  def countPairwiseIncreases(lines: Seq[Int]): Int = {
+    lines
+      .sliding(2)
+      .count(pair => pair.head < pair.last)
+  }
+
+  def part2(lines: Seq[Int]): Int = {
+    countPairwiseIncreases(
+      lines
+        .sliding(3)
+        .map(_.sum)
+        .toSeq
+    )
+  }
+
+  val lines = io.Source.fromResource("day1.txt").getLines().toSeq.map(_.toInt)
+
+  println(s"Part 1 = ${countPairwiseIncreases(lines)}")
+  println(s"Part 2 = ${part2(lines)}")
+}
